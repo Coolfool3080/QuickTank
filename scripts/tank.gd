793dca180f4aa2322978f012cff58901d15ab19b
Captchapro <95158386+CaptchaPro@users.noreply.github.com>
2025-05-24 12:51:06 -0700

    Implemented basic tank movement
    - Added custom WASD game key binds
    - removed the ability to jump with tank
    - created classic tank movement system (same as what we did last time)

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,28 +1,25 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+const MOVE_SPEED = 5
+const ROTATION_SPEED = 1
 
 func _physics_process(delta):
-	# Add the gravity.
+	#Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var rotation_dir = Input.get_axis("tank_rotate_right", "tank_rotate_left")
+	var movement_dir = Input.get_axis("tank_backward", "tank_forward")
+	
+	if Input.is_action_pressed("tank_forward"):
+		rotate_y(rotation_dir * delta)
+		velocity = global_transform.basis.z.normalized() * MOVE_SPEED
+	elif Input.is_action_pressed("tank_backward"):
+		rotate_y(-rotation_dir * delta)
+		velocity = -global_transform.basis.z.normalized() * MOVE_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		rotate_y(rotation_dir * delta)
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MOVE_SPEED)
 
 	move_and_slide()
