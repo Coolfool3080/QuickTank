0087dfd01a477941b913c9b47f977755ff469b55
Aaron M. Arreola <139318309+Coolfool3080@users.noreply.github.com>
2025-05-28 11:21:40 -0700

    Updated new tank/controller system
    Just another attempt at getting tank controls to feel correct, need to work on networking with this new model.
    
    -new level added that test the "new" tank, level name is "level_11"

--- assets/tank_movement_controller.gd
+++ assets/tank_movement_controller.gd
@@ -1,28 +1,52 @@
 extends CharacterBody3D
 
-const MOVE_SPEED = 5
-const ROTATION_SPEED = 1
+@onready var camera_pivot = $camera_pivot
+@onready var head_node = $head
+
+const MOVE_SPEED = 5.0
+const ROTATION_SPEED = 1.5
+const CAMERA_SENSITIVITY = 0.001
+const HEAD_ROTATE_SPEED = 1.0
+
+var camera_yaw = 0.0
+
+func _ready():
+	$camera_pivot/Camera3D.current = is_multiplayer_authority()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		camera_yaw -= event.relative.x * CAMERA_SENSITIVITY
+		camera_pivot.rotation.y = camera_yaw
 
 func _physics_process(delta):
 	if not is_multiplayer_authority():
 		return
-	
-	var direction = Vector3.ZERO
-	
-	if Input.is_action_pressed("tank_rotate_right"):
-		rotate_y(-.05)
-	if Input.is_action_pressed("tank_rotate_left"):
-		rotate_y(.05)
-	if Input.is_action_pressed("tank_backward"):
-		velocity.z += 1
-	if Input.is_action_pressed("tank_forward"):
-		velocity.z -= 1
-	
-	if velocity != Vector3.ZERO:
-		velocity = velocity.normalized() * MOVE_SPEED
-	
-	# Gravity
+
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
+		velocity.y += get_gravity().y * delta
+	else:
+		velocity.y = 0
+
+	var turn_input = Input.get_axis("tank_rotate_right", "tank_rotate_left")
+	if turn_input != 0:
+		rotate_y(turn_input * ROTATION_SPEED * delta)
+
+	var move_input = Input.get_axis("tank_backward", "tank_forward")
+	if move_input != 0:
+		var forward = -global_transform.basis.z.normalized()
+		velocity.x = forward.x * MOVE_SPEED * move_input
+		velocity.z = forward.z * MOVE_SPEED * move_input
+	else:
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED/2 * delta)
+		velocity.z = move_toward(velocity.z, 0, MOVE_SPEED/2 * delta)
+
 	move_and_slide()
+
+	# Rotate head relative to tank's local space
+	var tank_basis = global_transform.basis
+	var camera_global_yaw = camera_pivot.global_transform.basis.get_euler().y
+	var tank_yaw = tank_basis.get_euler().y
+	var relative_yaw = camera_global_yaw - tank_yaw
+
+	head_node.rotation.y = lerp_angle(head_node.rotation.y, relative_yaw, delta * HEAD_ROTATE_SPEED)
