b1f7bca499e6f8584c197a62d2c00a361cf8c233
Captchapro <95158386+CaptchaPro@users.noreply.github.com>
2025-05-28 00:16:43 -0700

    Added ability for the tank head to move with look angle
    - tank head and barrel now looks in the direction where the camera is looking,
    this was achieved by moving the barrel off to the side, a more elegant solution is desired

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -4,18 +4,20 @@ const MOVE_SPEED = 5
 const ROTATION_SPEED = 1
 
 func _physics_process(delta):
-	
-
 	if not is_multiplayer_authority():
 		return
 
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	var tank_head = get_parent().get_node("tank/tank_head_phys_box")
+	var look_pos = get_parent().get_node("tank/camera_controller/look_node").global_position
+	
+	tank_head.look_at(look_pos)
 
 	# Input
 	var rotation_dir = Input.get_axis("tank_rotate_right", "tank_rotate_left")
-	var movement_dir = Input.get_axis("tank_backward", "tank_forward")
 
 	if Input.is_action_pressed("tank_forward"):
 		rotate_y(rotation_dir * delta)
