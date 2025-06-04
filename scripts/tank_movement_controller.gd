2fce8d417819d83e7e0191b5fb36191a8d3ae394
Aaron M. Arreola <139318309+Coolfool3080@users.noreply.github.com>
2025-06-04 16:15:44 -0700

    Second crosshair
    -added second crosshair that move more quickly with the camera

--- scripts/tank_movement_controller.gd
+++ scripts/tank_movement_controller.gd
@@ -2,7 +2,9 @@ extends CharacterBody3D
 
 @onready var camera_pivot = $camera_pivot
 @onready var head_node = $head
+@onready var fast_head = $head2
 @onready var head_barrel = $head/head_mesh/barrel_pivot
+@onready var fast_barrel = $head2/barrel_pivot2
 @onready var fire_cooldown_timer = $fire_cooldown_timer
 
 @export var Bullet: PackedScene
@@ -10,7 +12,7 @@ extends CharacterBody3D
 const MOVE_SPEED = 5.0
 const ROTATION_SPEED = 1.5
 const CAMERA_SENSITIVITY = 0.001
-const HEAD_ROTATE_SPEED = 1.0
+const HEAD_ROTATE_SPEED = 2.0
 
 var TANK_HEALTH = 5
 var camera_yaw = 0.0
@@ -72,6 +74,10 @@ func _physics_process(delta):
 	
 	head_node.rotation.y = lerp_angle(head_node.rotation.y, relative_yaw, delta * HEAD_ROTATE_SPEED)
 	head_barrel.rotation.x = lerp_angle(head_barrel.rotation.x, relative_pitch, delta * HEAD_ROTATE_SPEED)
+	
+	
+	fast_head.rotation.y = relative_yaw
+	fast_barrel.rotation.x = relative_pitch
 
 	var barrel = $head/head_mesh/barrel_pivot
 	var camera = get_viewport().get_camera_3d()
@@ -99,6 +105,31 @@ func _physics_process(delta):
 	else:
 		$hud/Line2D.visible = true
 		$hud/Line2D.position = crosshair_pos
+	
+	var barrel2 = $head2/barrel_pivot2
+	var camera2 = get_viewport().get_camera_3d()
+	var aim_origin2 = barrel2.global_transform.origin
+	var aim_direction2 = -barrel2.global_transform.basis.z
+	var target_point2 = aim_origin2 + aim_direction2 * 1000
+	
+	var query2 = PhysicsRayQueryParameters3D.new()
+	query2.from = aim_origin2
+	query2.to = target_point2
+	query2.collision_mask = 1
+	
+	var space_state2 = get_world_3d().direct_space_state
+	var result2 = space_state2.intersect_ray(query2)
+	var crosshair_pos2 = Vector2.ZERO
+	if result2:
+		crosshair_pos2 = camera2.unproject_position(result2.position)
+	else:
+		crosshair_pos2 = camera2.unproject_position(target_point2)
+
+	if camera2.is_position_behind(target_point2):
+		$hud/crosshair.visible = false
+	else:
+		$hud/crosshair.visible = true
+		$hud/crosshair.position = crosshair_pos2
 	
 	#shoot
 func _input(event):
